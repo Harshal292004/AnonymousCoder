f9bf7964b843a3b958fceff33ede1972b7acd801
Harshal292004 <malaniharshal95@gmail.com>
2025-08-21 10:29:45 +0530

    nodes factory

--- src/agent_project/core/graph/nodes.py
+++ src/agent_project/core/graph/nodes.py
@@ -1,15 +1,15 @@
 from ..states.AnonymousState import AnonymousState
 from prompts.system_prompt import get_system_prompt
 from infrastructure.llm_clients.llms import GroqLLM,LLMConfig
-
-def get_memory_node():
+from langchain_core.language_models import BaseChatModel
+def get_memory_node(llm:BaseChatModel):
+    def memory_node(state:AnonymousState):
+        query=state.messages[-1]
+        llm.invoke([query])
         
-def memory_node(state:AnonymousState):
-    query=state.messages[-1]
-    GroqLLM().create_llm(config=LLMConfig(provider="groq"))
-    
-    
-    pass
+        pass
+
+    return memory_node
 
 def understand_query_node(state:AnonymousState):
     
