19e6448d1f2c2d5a443b0beb15ee8aa68cc5a94a
Harshal292004 <malaniharshal95@gmail.com>
2025-08-21 23:25:29 +0530

    add

--- src/agent_project/core/graph/nodes.py
+++ src/agent_project/core/graph/nodes.py
@@ -3,13 +3,13 @@ from prompts.system_prompt import get_system_prompt
 from infrastructure.llm_clients.llms import GroqLLM,LLMConfig
 from langchain_core.language_models import BaseChatModel
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-
+from prompts.system_prompt import 
 def get_memory_node(embeddings_llm:HuggingFaceEmbeddings,llm:BaseChatModel):
     def memory_node(state:AnonymousState):
         query=state.messages[-1]
         llm_with_tools=llm.bind_tools([])
         
-        memory_ananlysis_chain= prompt | embeddings_llm
+        memory_ananlysis_chain=
         
         pass
 
